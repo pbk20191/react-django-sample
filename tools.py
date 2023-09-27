2da4c0405b88f75a3408f1e38bd97aaeffc9e86a
park-byeong-gwan <impbk2002@gmail.com>
2023-09-27 23:04:42 +0900

    implement tools.py on macOS side

--- tools.py
+++ tools.py
@@ -1,20 +1,27 @@
 from os.path import join
-import os, asyncio
+import os, asyncio, venv
 
 async def setup_python():
-    dir = join(os.curdir, ".venv")
-    python_command = "python3"
+    dir = join(os.getcwd(), ".venv")
     if os.name == 'nt':
-        python_command = 'py'
-    venv_process = await asyncio.create_subprocess_exec(python_command, "-m", "venv", ".venv", cwd=os.curdir)
-    await venv_process.wait()
+        await asyncio.to_thread(lambda: venv.create(".venv", with_pip=True))
+    else:
+        venv_process = await asyncio.create_subprocess_exec("python3", "-m", "venv", ".venv", cwd=os.getcwd())
+        await venv_process.wait()
     if os.name == 'nt':
-        process = await asyncio.create_subprocess_exec(program=".venv\\Scripts\\activate.bat", cwd=os.curdir)
+        process = await asyncio.create_subprocess_shell(
+            executable="C:\\Windows\\system32\\cmd.exe",
+            cmd=".\\.venv\\Scripts\\activate.bat /d /c",
+            cwd=os.getcwd()
+        )
         await process
     else:
         process = await asyncio.create_subprocess_shell(cmd="source .venv/bin/activate", cwd=os.curdir)
         await process.wait()
-    process = await asyncio.create_subprocess_exec(".venv/bin/" + python_command, *["-m", "pip", "install", "-r", "requirements.txt"], cwd=os.curdir)
+    python_command = join(".venv", "bin", "python3")
+    if os.name == 'nt':
+        python_command = join(".venv", "Scripts", "python.exe")
+    process = await asyncio.create_subprocess_exec(python_command, *["-m", "pip", "install", "-r", "requirements.txt"], cwd=os.getcwd())
     await process.wait()
 
 async def setup_node():
