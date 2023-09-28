90c8a4bb96fc5834783f5c28b39c3a8de2410f27
park-byeong-gwan <impbk2002@gmail.com>
2023-09-28 12:42:16 +0900

    setup task 간소화 및 tools.py 포맷팅 수정

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@ import os, asyncio, venv
 from argparse import ArgumentParser
 from pathlib import Path
 
-async def create_venv(name:str = ".venv"):
+async def create_venv(name: str = ".venv"):
     '''
     파이썬 가상환경을 생성합니다.
 
@@ -14,7 +14,7 @@ async def create_venv(name:str = ".venv"):
         process = await asyncio.create_subprocess_exec("python3", "-m", "venv", name, cwd=Path.cwd())
         await process.wait()
 
-async def activate_venv(name:str = ".venv"):
+async def activate_venv(name: str = ".venv"):
     '''
     파이썬 가상환경을 활성화합니다.
 
@@ -26,14 +26,19 @@ async def activate_venv(name:str = ".venv"):
             cmd=f"cmd /c {path} /d",
             cwd=Path.cwd()
         )
+        # path = Path(".", name, "Scripts", "Activate.ps1")
+        # process = await asyncio.create_subprocess_shell(
+        #     cmd=f"powershell.exe {path}",
+        #     cwd=Path.cwd()
+        # )
         await process.wait()
     else:
         path = Path(name, "bin", "activate")
         process = await asyncio.create_subprocess_shell(cmd=f"source {path}", cwd=Path.cwd())
         await process.wait()
 
-def python_executable(name:str = ".venv") -> Path: 
-    path: Path | None = None
+def python_executable(name: str = ".venv") -> Path: 
+    path: Path = None
     if os.name == 'nt':
         path = Path(name, "Scripts", "python.exe")
     else:
@@ -60,15 +65,6 @@ async def setup_all():
     async with asyncio.TaskGroup() as group:
         group.create_task(setup_node())
         group.create_task(setup_python())
-
-def main_command(inputs:[str]):
-    root_parser = ArgumentParser(
-        description="프로젝트 세팅 및 실행을 도와주는 스크립트입니다."
-    )
-    group_parser = root_parser.add_subparsers(help="functions")
-    command_create = group_parser.add_parser("create", help="create project envrionment")
-    
-    result = root_parser.parse_args()
     
 
 if __name__ == '__main__':
