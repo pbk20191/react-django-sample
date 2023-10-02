003a78e9775f0e64fe0b90ac860c9de6f899f008
park-byeong-gwan <impbk2002@gmail.com>
2023-10-02 11:30:39 +0900

    reduce codes in csrf fetch

--- client/src/utils/csrf_fetch.ts
+++ client/src/utils/csrf_fetch.ts
@@ -18,32 +18,21 @@ window.fetch = async (input, init) => {
     const csrf_cookie = getCookie('csrftoken')
     if (csrf_cookie == null || csrf_cookie == undefined) {
         return await orgin_fetch(input, init)
-    } else if (init == undefined) {
-        let headers = new Headers()
-        if (input instanceof Request) {
-            headers = input.headers
-        }
-        headers.set("X-CSRFToken", csrf_cookie)
-        init = { headers: headers }
-    } else if (init.headers == undefined) {
-        let headers = new Headers()
-        if (input instanceof Request) {
-            headers = input.headers
-        }
-        headers.set("X-CSRFToken", csrf_cookie)
-        init.headers = headers
-    } else {
-        if (init.headers instanceof Headers) {
-            init.headers.append("X-CSRFToken", csrf_cookie)
-        } else if (init.headers instanceof Array) {
-            const target = init.headers.find(value => value[0].toUpperCase() === "X-CSRFToken")
-            if (target) {
-                target[1] = csrf_cookie
-            } else {
-                init.headers.push(["X-CSRFToken", csrf_cookie])
-            }
+    } else if (input instanceof Request) {
+        if (init != undefined && init.headers != undefined) {
+            const newHeaders = new Headers(init.headers)
+            newHeaders.set("X-CSRFToken", csrf_cookie)
+            init.headers = newHeaders
         } else {
-            Object.assign(init.headers, {"X-CSRFToken": csrf_cookie})
+            input.headers.set("X-CSRFToken", csrf_cookie)
+        }
+    } else {
+        const newHeaders = new Headers(init?.headers)
+        newHeaders.set("X-CSRFToken", csrf_cookie)
+        if (init == undefined) {
+            init = { headers: newHeaders }
+        } else {
+            init.headers = newHeaders
         }
     }
     return await orgin_fetch(input, init)
